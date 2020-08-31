d8589b85395c21ea1f26c63a307eff7dbfaa9c62
Trevor Davis <trevormckay.davis@gmail.com>
2020-08-31 16:48:34 -0500

    primary objective scorring logic

--- src/player.svelte
+++ src/player.svelte
@@ -1,6 +1,6 @@
 <script>
   const playerData = {
-    name: "Steve",
+    name: "",
     cp: 0,
     faction: "",
     primary: [0, 0, 0, 0],
@@ -12,6 +12,17 @@
   };
 
   export let playerID;
+
+  function scorePrimaries(turn, newScore) {
+    playerData.primary = playerData.primary.map((currentScore, idx) =>
+      turn === idx ? (newScore === currentScore ? 0 : newScore) : currentScore
+    );
+  }
+
+  $: primariesScore = playerData.primary.reduce((total, score) => {
+    let newScore = total + score;
+    return newScore >= 45 ? 45 : newScore;
+  });
 </script>
 
 <style>
@@ -21,6 +32,9 @@
     height: 100%;
     justify-content: space-between;
     align-content: space-between;
+  }
+  .selected {
+    background-color: blueviolet;
   }
 </style>
 
@@ -38,66 +52,71 @@
     <input id="faction" type="string" />
   </div>
   <p>Primary turn 2</p>
-  <div>
-    <input type="radio" id="5" name="drone" value={5} />
-    <label for="5">5</label>
-  </div>
-
-  <div>
-    <input type="radio" id="10" name="drone" value={10} />
-    <label for="10">10</label>
-  </div>
-
-  <div>
-    <input type="radio" id="15" name="drone" value={15} />
-    <label for="15">15</label>
-  </div>
+  <button
+    class={playerData.primary[0] === 5 ? 'selected' : 'unselected'}
+    on:click={() => scorePrimaries(0, 5)}>
+    5
+  </button>
+  <button
+    class={playerData.primary[0] === 10 ? 'selected' : 'unselected'}
+    on:click={() => scorePrimaries(0, 10)}>
+    10
+  </button>
+  <button
+    class={playerData.primary[0] === 15 ? 'selected' : 'unselected'}
+    on:click={() => scorePrimaries(0, 15)}>
+    15
+  </button>
   <p>Primary turn 3</p>
-  <div>
-    <input type="radio" id="5" name="drone" value={5} />
-    <label for="5">5</label>
-  </div>
-
-  <div>
-    <input type="radio" id="10" name="drone" value={10} />
-    <label for="10">10</label>
-  </div>
-
-  <div>
-    <input type="radio" id="15" name="drone" value={15} />
-    <label for="15">15</label>
-  </div>
+  <button
+    class={playerData.primary[1] === 5 ? 'selected' : 'unselected'}
+    on:click={() => scorePrimaries(1, 5)}>
+    5
+  </button>
+  <button
+    class={playerData.primary[1] === 10 ? 'selected' : 'unselected'}
+    on:click={() => scorePrimaries(1, 10)}>
+    10
+  </button>
+  <button
+    class={playerData.primary[1] === 15 ? 'selected' : 'unselected'}
+    on:click={() => scorePrimaries(1, 15)}>
+    15
+  </button>
   <p>Primary turn 4</p>
-  <div>
-    <input type="radio" id="5" name="drone" value={5} />
-    <label for="5">5</label>
-  </div>
-
-  <div>
-    <input type="radio" id="10" name="drone" value={10} />
-    <label for="10">10</label>
-  </div>
-
-  <div>
-    <input type="radio" id="15" name="drone" value={15} />
-    <label for="15">15</label>
-  </div>
+  <button
+    class={playerData.primary[2] === 5 ? 'selected' : 'unselected'}
+    on:click={() => scorePrimaries(2, 5)}>
+    5
+  </button>
+  <button
+    class={playerData.primary[2] === 10 ? 'selected' : 'unselected'}
+    on:click={() => scorePrimaries(2, 10)}>
+    10
+  </button>
+  <button
+    class={playerData.primary[2] === 15 ? 'selected' : 'unselected'}
+    on:click={() => scorePrimaries(2, 15)}>
+    15
+  </button>
   <p>Primary turn 5</p>
-  <div>
-    <input type="radio" id="5" name="drone" value={5} />
-    <label for="5">5</label>
-  </div>
-
-  <div>
-    <input type="radio" id="10" name="drone" value={10} />
-    <label for="10">10</label>
-  </div>
-
-  <div>
-    <input type="radio" id="15" name="drone" value={15} />
-    <label for="15">15</label>
-  </div>
+  <button
+    class={playerData.primary[3] === 5 ? 'selected' : 'unselected'}
+    on:click={() => scorePrimaries(3, 5)}>
+    5
+  </button>
+  <button
+    class={playerData.primary[3] === 10 ? 'selected' : 'unselected'}
+    on:click={() => scorePrimaries(3, 10)}>
+    10
+  </button>
+  <button
+    class={playerData.primary[3] === 15 ? 'selected' : 'unselected'}
+    on:click={() => scorePrimaries(3, 15)}>
+    15
+  </button>
   <p>Total Primary Points</p>
+  <p>{primariesScore}</p>
   <p>Secondary 1</p>
   <p>Secondary 2</p>
   <p>Secondary 3</p>
