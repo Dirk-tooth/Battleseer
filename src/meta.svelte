7e3cb5bfb28ce5cd8069c57bc217187bd164b93a
Trevor Davis <trevormckay.davis@gmail.com>
2020-09-02 13:05:28 -0500

    some meta data styles

--- src/meta.svelte
+++ src/meta.svelte
@@ -7,19 +7,40 @@
     /* background-color: chartreuse; */
     width: 100%;
   }
+  .name {
+    width: 100%;
+    padding: 0 1rem;
+  }
+  .name input {
+    width: auto;
+  }
+  .second-row {
+    display: flex;
+  }
+  .cp {
+    width: 25%;
+  }
+  .cp input {
+    width: 20%;
+  }
+  .faction {
+    width: 80%;
+  }
 </style>
 
 <div class="meta">
-  <div>
+  <div class="name">
     <label for="player-name">Player name</label>
     <input id="player-name" type="string" value={name} />
   </div>
-  <div>
-    <label for="CP">CP</label>
-    <input id="CP" type="number" min="0" value={cp} />
-  </div>
-  <div>
-    <label for="faction">Faction</label>
-    <input id="faction" type="string" value={faction} />
+  <div class="second-row">
+    <div class="cp">
+      <label for="CP">CP</label>
+      <input id="CP" type="number" min="0" value={cp} />
+    </div>
+    <div class="faction">
+      <label for="faction">Faction</label>
+      <input id="faction" type="string" value={faction} />
+    </div>
   </div>
 </div>
